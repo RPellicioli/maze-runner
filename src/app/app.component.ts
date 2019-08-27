7c076a96db9440e7307ebdf79933c909296cb14e
Ricardo Pellicioli <pellicioli_r@hotmail.com>
2019-08-26 22:48:20 -0300

    build page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'maze-runner';
+    public maze: Array<number> = [];
+
+    constructor(){
+
+    }
+
+    public ngOnInit(): void {
+        for(let i = 0; i < 100; i++){
+            this.maze.push(i);
+        }
+    }
 }
