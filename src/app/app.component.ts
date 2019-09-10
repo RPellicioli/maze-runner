3d75813b524c8b5fd61dc12718b9a5d2ef01631b
Ricardo Pellicioli <pellicioli_r@hotmail.com>
2019-09-10 19:55:20 -0300

    find-fitness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,36 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
     public maze: Array<Block> = [];
     public eltismo: boolean = true;
     public crossover: number = 0.7;
     public mutation: number = 0.3;
-    public population: number = 100;
-    public maxGenerations: number = 1000;
-    public solution: Array<number>;
+    public populationLength: number = 100;
+    public maxGenerations: number = 100;
+    public solution: string = "010101000001010001101010010000000000010100000011000100";
+    public characters: Array<string> = ["00", "01", "10", "11"];
+    public genesTotal: number;
+    public log: Array<string> = [];
 
-    constructor(){
-
+    constructor() {
+        this.genesTotal = this.solution.length / 2;
     }
 
     public ngOnInit(): void {
-        for(let i = 0; i < 100; i++){
+        for (let i = 0; i < 100; i++) {
             this.maze.push(
                 {
                     id: i,
                     wLeft: false,
                     wRight: false,
                     wTop: false,
-                    wBottom: false
+                    wBottom: false,
+                    active: false
                 }
             );
         }
@@ -366,14 +370,240 @@ export class AppComponent {
     }
 
     public start(): void {
-        alert(this.crossover);
-    } 
+        let hasSolution = false;
+        let generation = 0;
+
+        this.log = [];
+
+        //cria a primeira população aleatória
+        let population = this.createPopulation(this.genesTotal, this.populationLength);
+        this.log.push("Iniciando... Aptidão da solução: " + this.solution.length);
+
+        //verifica se tem a solucao
+        hasSolution = this.verifyHasSolution(population);
+
+        while (!hasSolution && generation < this.maxGenerations) {
+            generation++;
+
+            //cria nova populacao
+            population = this.newGeneration(population, this.eltismo);
+
+            this.log.push("Geração " + generation + " | Aptidão: " + population.individuos[0].fitness + " | Melhor: " + population.individuos[0].genes);
+
+            //verifica se tem a solucao
+            hasSolution = this.verifyHasSolution(population);
+        }
+
+        if (generation == this.maxGenerations) {
+            this.log.push("Atingiu o número máximo de gerações | " + population.individuos[0].genes + " " + population.individuos[0].fitness);
+        }
+
+        if (hasSolution) {
+            this.log.push("Encontrado resultado na geração " + generation + " | " + population.individuos[0].genes + " (Aptidão: " + population.individuos[0].fitness + ")");
+        }
+    }
+
+    private verifyHasSolution(population: Population): boolean {
+        let individuo: Individuo = null;
+
+        population.individuos.forEach(i => {
+            if (i.genes == this.solution) {
+                individuo = i;
+                return;
+            }
+        });
+
+        return individuo == null ? false : true;
+    }
+
+    private newGeneration(population: Population, elitismo: boolean): Population {
+        //nova população do mesmo tamanho da antiga
+        let newPopulation = new Population();
+        newPopulation.individuos = [];
+
+        population.individuos.sort((a, b) => (a.fitness < b.fitness) ? 1 : -1);
+
+        //se tiver elitismo, mantém o melhor indivíduo da geração atual
+        if (elitismo) {
+            newPopulation.individuos.push(population.individuos[0]);
+        }
+
+        //insere novos indivíduos na nova população, até atingir o tamanho máximo
+        while (newPopulation.individuos.length < this.populationLength) {
+            //seleciona os 2 pais por torneio
+            let parents = this.tournament(population);
+
+            let children = [];
+
+            //verifica a taxa de crossover, se sim realiza o crossover, se não, mantém os pais selecionados para a próxima geração
+            if (this.randomDoubleFromInterval(0, 100) <= this.crossover) {
+                children = this.generateCrossover(parents[1], parents[0]);
+            }
+            else {
+                children.push(parents[0]);
+                children.push(parents[1]);
+            }
+
+            //adiciona os filhos na nova geração
+            newPopulation.individuos.push(children[0]);
+            newPopulation.individuos.push(children[1]);
+        }
+
+        //ordena a nova população
+        newPopulation.individuos.sort((a, b) => (a.fitness < b.fitness) ? 1 : -1)
+        return newPopulation;
+    }
+
+    public tournament(population: Population): Array<Individuo> {
+        let newPopulation = new Population();
+        newPopulation.individuos = [];
+
+        //seleciona 3 indivíduos aleatóriamente na população
+        newPopulation.individuos.push(population.individuos[this.randomIntFromInterval(0, this.populationLength - 1)]);
+        newPopulation.individuos.push(population.individuos[this.randomIntFromInterval(0, this.populationLength - 1)]);
+        newPopulation.individuos.push(population.individuos[this.randomIntFromInterval(0, this.populationLength - 1)]);
+
+        //ordena a população
+        newPopulation.individuos.sort((a, b) => (a.fitness < b.fitness) ? 1 : -1);
+
+        let parents: Array<Individuo> = [];
+
+        //seleciona os 2 melhores deste população
+        parents.push(newPopulation.individuos[0]);
+        parents.push(newPopulation.individuos[1]);
+
+        return parents;
+    }
+
+    public generateCrossover(individuo1: Individuo, individuo2: Individuo) {
+        //sorteia o ponto de corte
+        let pontoCorte1 = this.randomIntFromInterval(0, (individuo1.genes.length / 2) - 2) + 1;
+        let pontoCorte2 = this.randomIntFromInterval(0, (individuo1.genes.length / 2) - 2) + individuo1.genes.length / 2;
+
+        let children = [];
+
+        //pega os genes dos pais
+        let parentGene1 = individuo1.genes;
+        let parentGene2 = individuo2.genes;
+
+        //realiza o corte, 
+        let childGene1 = parentGene1.substr(0, pontoCorte1);
+        childGene1 = childGene1 + parentGene2.substr(pontoCorte1, (pontoCorte2 - pontoCorte1));
+        childGene1 = childGene1 + parentGene1.substr(pontoCorte2, (parentGene1.length - pontoCorte2));
+
+        let childGene2 = parentGene2.substr(0, pontoCorte1);
+        childGene2 = childGene2 + parentGene1.substr(pontoCorte1, (pontoCorte2 - pontoCorte1));
+        childGene2 = childGene2 + parentGene2.substr(pontoCorte2, (parentGene2.length - pontoCorte2));
+
+        //cria o novo indivíduo com os genes dos pais
+        children.push(this.createIndividuoByGenes(childGene1));
+        children.push(this.createIndividuoByGenes(childGene2));
+
+        return children;
+    }
+
+    private createPopulation(genesTotal: number, length: number): Population {
+        let population = new Population();
+        population.length = length;
+
+        population.individuos = [];
+        for (let i = 0; i < length; i++) {
+            population.individuos[i] = this.createIndividuo(genesTotal);
+        }
+
+        return population;
+    }
+
+    private createIndividuo(genesTotal: number): Individuo {
+        let individuo = new Individuo();
+        individuo.genes = "";
+        individuo.fitness = 0;
+
+        for (let i = 0; i < genesTotal; i++) {
+            var index = this.randomIntFromInterval(0, 3);
+            individuo.genes = individuo.genes + this.characters[index];
+        }
+
+        this.generateFitness(individuo);
+
+        return individuo;
+    }
+
+    private createIndividuoByGenes(genes: string): Individuo {
+        let individuo = new Individuo();
+        individuo.genes = genes;
+
+        //se for mutar, cria um gene aleatório
+        if (this.randomDoubleFromInterval(0, 100) <= this.mutation) {
+            let geneNovo = "";
+            let posAleatoria = this.randomIntFromInterval(0, genes.length / 2);
+
+            if (posAleatoria % 2 != 0) {
+                posAleatoria--;
+            }
+
+            if (posAleatoria == 54) {
+                posAleatoria--;
+            }
+
+            for (let i = 0; i < genes.length; i++)
+            {
+                if (i == posAleatoria + 1) continue;
+
+                if (i == posAleatoria) {
+                    geneNovo = geneNovo + this.characters[this.randomIntFromInterval(0, this.characters.length)];
+                }
+                else {
+                    geneNovo = geneNovo + genes[i];
+                }
+
+            }
+            individuo.genes = geneNovo;
+        }
+
+        this.generateFitness(individuo);
+
+        return individuo;
+    }
+
+    private randomIntFromInterval(min, max): number { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    private randomDoubleFromInterval(min, max): number { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min) / 100;
+    }
+
+    private generateFitness(individuo: Individuo): void {
+        if(!individuo.fitness) individuo.fitness = 0;
+
+        for (let i = 0; i < this.solution.length; i += 2) {
+            var path = this.solution[i] + this.solution[i + 1];
+            var genesPath = individuo.genes[i] + individuo.genes[i + 1];
+
+            if (path == genesPath) {
+                individuo.fitness++;
+            }
+        }
+    }
 }
 
-export class Block{
+export class Block {
     public id: number;
     public wLeft: boolean;
     public wRight: boolean;
     public wTop: boolean;
     public wBottom: boolean;
+    public active: boolean;
 }
+
+export class Population {
+    public individuos: Array<Individuo>;
+    public length: number;
+}
+
+export class Individuo {
+    public genes: string;
+    public fitness: number;
+}
+
